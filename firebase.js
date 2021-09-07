b6b7a7c36dc1df620d944fd446de83c2dab5408e
Steve Chen <schen@Steves-MacBook-Air.local>
2021-09-06 18:29:29 -0700

    amazon-clone

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmaR_V1ZUVNbjiWFskkIrnfBPtJZNNvC0",
+    authDomain: "clone-e5a81.firebaseapp.com",
+    projectId: "clone-e5a81",
+    storageBucket: "clone-e5a81.appspot.com",
+    messagingSenderId: "707118958171",
+    appId: "1:707118958171:web:95ecf7036f3d37217e617e"
+  };
+
+  /*
+  const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+  const db = app.firestore();
+
+  export default db;
+  */
---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Image from "next/image";
+import {StarIcon} from "@heroicons/react/solid";
+import Currency from "react-currency-formatter"
+import { useDispatch } from 'react-redux';
+import {addToBasket, removeFromBasket} from "../slices/basketSlice";
+
+function CheckoutProduct({id, title, price, rating, description, category, image, hasPrime}) {
+    const dispatch = useDispatch();
+    const addItemToCart = () => {
+        const product = {
+            id, title, price, rating, description, category, image, hasPrime
+        }
+        dispatch(addToBasket(product));
+    }
+    const removeItemFromCart = () => {
+        dispatch(removeFromBasket({id}));
+    }
+    return (
+        <div className="grid grid-cols-5">
+            <Image src={image}
+            height={200}
+            width={200}
+            objectFit="contain"
+            />
+            <div className="col-span-3 mx-5">
+                <p>{title}</p>
+                <div className="flex">
+                    {Array(rating)
+                    .fill()
+                    .map((_,i)=>(
+                        <StarIcon key={i} className="h-5 text-yellow-500" />
+                    ))}
+                </div>
+                <p className="text-xs my-2 line-clamp-3">{description}</p>
+                <Currency quantity={price} currency='USD' />
+                {hasPrime && (
+                    <div className="flex items-center space-x-2">
+                        <img src="https://links.papareact.com/fdw"
+                        alt=""
+                        className="w-12" 
+                        loading="lazy"/>
+                    <p className="text-xs text-gray-500">FREE 2-day Delivery</p>
+                    </div>
+                )}
+            </div>
+            <div className="flex flex-col space-y-2 my-auto justify-center">
+                <button className="button"
+                onClick={addItemToCart}>Add to Cart</button>
+                <button className="button"
+                onClick={removeItemFromCart}>Remove from Cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
