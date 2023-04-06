8055a4bf4821914b6ea74dcb01980c55810ec6ae
Steve Chen <87613740+schen7239@users.noreply.github.com>
2023-04-05 21:40:55 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "clone-325614.firebaseapp.com",
-  projectId: "amazon-clone-325614",
-  storageBucket: "amazon-clone-325614.appspot.com",
-  messagingSenderId: "117274096279",
-  appId: "1:117274096279:web:3717451fa058a1bd96afd2"
 };
 
   
