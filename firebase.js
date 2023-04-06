bca0661490e58279093ea94122df358efe0e77b4
Steve Chen <87613740+schen7239@users.noreply.github.com>
2023-04-05 21:54:16 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,6 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
   
